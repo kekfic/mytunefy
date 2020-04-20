9ce19f304e8aeef54b5d1db1db6b60e730d2d511
Francesco Fico <kekko.fico.ff@gmail.com>
2020-04-20 22:10:25 +0200

    deleting becouse of failure

--- resources/resources.py
+++
@@ -1,6 +0,0 @@
-from queue import Queue
-import  datetime
-songPusher = Queue()
-
-myEndtime = datetime.datetime(2020, 5, 15, 0, 0).timestamp()
-myStarttime = datetime.datetime(2020, 5, 1, 0, 0).timestamp()
