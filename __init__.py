2aad165771da9f567ab019853381b106f9a079bf
Francesco Fico <kekko.fico.ff@gmail.com>
2020-04-24 00:23:11 +0200

    __init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-__version__ = "0.2.0"
-#
+__version__ = "0.2.8"
+__notes__ = "released 24 Abril 2020"
+__author__ = "np1"
+__license__ = "GPLv3"
+__url__ = "https://"#
 
 """ I am using the __init__.py file in the wrong way
     Sometimes it happens...
