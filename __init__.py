b84b303420f5478be42c36fa499ecab0a4e8ef50
kekfic <45541827+kekfic@users.noreply.github.com>
2020-05-09 20:47:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.2.8"
-__notes__ = "released 24 Abril 2020"
+__version__ = "0.0.1"
+__notes__ = "Not realeased"
 __author__ = "cesk"
 __license__ = "GPLv3"
 __url__ = "https://"#
@@ -7,5 +7,5 @@ __url__ = "https://"#
 
 """ I am using the __init__.py file in the wrong way
     Sometimes it happens...
-    # Todo ---- Modify here
+    
 """
